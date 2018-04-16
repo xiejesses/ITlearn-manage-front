6251d23bcd444b70c5e2224ef5ff7b56ae3a1360
YangXiaos <178069857@qq.com>
2018-04-16 16:04:30 +0800

    1. 配置代理 2. review

--- config/index.js
+++ config/index.js
@@ -10,7 +10,6 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -19,8 +18,12 @@ module.exports = {
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+    proxyTable: {
+      '/v1':{
+        target:'http://localhost:3000'
+      }
+    },
 
-    
     /**
      * Source Maps
      */
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/components/Home'
-import User from '@/components/User'
-import Recommend from '@/components/Recommend'
-import Tag from '@/components/Tag'
-import Group from '@/components/Group'
-import Topic from '@/components/Topic'
-import Comments from '@/components/Comments'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/components/Home';
+import User from '@/components/User';
+import Recommend from '@/components/Recommend';
+import Tag from '@/components/Tag';
+import Group from '@/components/group';
+import Topic from '@/components/Topic';
+import Comments from '@/components/Comments';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -18,34 +18,34 @@ export default new Router({
       component: Home,
     },
     {
-      path:'/user',
-      name:'user',
-      component:User
+      path: '/user',
+      name: 'user',
+      component: User
     },
     {
-      path:'/recommend',
-      name:'recommend',
-      component:Recommend
+      path: '/recommend',
+      name: 'recommend',
+      component: Recommend
     },
     {
-      path:'/tag',
-      name:'tag',
-      component:Tag
+      path: '/tag',
+      name: 'tag',
+      component: Tag
     },
     {
-      path:'/group',
-      name:'group',
-      component:Group
+      path: '/group',
+      name: 'group',
+      component: Group
     },
     {
-      path:'/topic',
-      name:'topic',
-      component:Topic
+      path: '/topic',
+      name: 'topic',
+      component: Topic
     },
     {
-      path:'/comments',
-      name:'comments',
-      component:Comments
+      path: '/comments',
+      name: 'comments',
+      component: Comments
     }
   ]
 })
